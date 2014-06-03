add53ff7e1930a204998cd969155419a3e0e802c
Kent Howard <kenthow@gmail.com>
2014-06-03 18:32:39 -0500

    third commit

--- etch.js
+++ etch.js
@@ -1,13 +1,16 @@
 $(document).ready(function(){
-	$('div').click(mkDiv());
+	mkDiv();
 });
+
 
 
 mkDiv = function(){
 	$grid = $('.grid');
 	for(var j = 0; j < 16; j++){
-	for(var i = 0; i < 16; i++){
-		$grid.append("<div class='square'></div>")
-	}
-	}
+		for(var i = 0; i < 16; i++){
+			$grid.append("<div class='square'></div>")
+		};
+		$grid.append('<div class="new_row"></div>')
+	};
+
 };
