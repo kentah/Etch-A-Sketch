149c85e345b57cc026533dcc8a9ca6088ec7e45d
Kent Howard <kenthow@gmail.com>
2014-06-08 22:07:39 -0500

    third commit

--- etch.js
+++ etch.js
@@ -1,16 +1,40 @@
 $(document).ready(function(){
-	mkDiv();
+    var input = 64;
+    $('#butt').click(function(){
+	$('.square').remove();
+	input = parseInt(prompt('Give me a number between 1 and 100'))
+	if(input < 1 || input > 100){
+	    input = parseInt(prompt("Try again, I'm not stupid!"));
+	}
+	mkDiv(input);
+	chngClr();
+    });
+
+    mkDiv(input);
+    chngClr();
 });
 
 
-
-mkDiv = function(){
-	$grid = $('.grid');
-	for(var j = 0; j < 16; j++){
-		for(var i = 0; i < 16; i++){
-			$grid.append("<div class='square'></div>")
-		};
-		$grid.append('<div class="new_row"></div>')
+mkDiv = function(boxCount){
+    for (var i = 0; i <= boxCount/2; i++){
+	for (var j = 0; j <= boxCount/2; j++){
+	    $('<div>', {'class' : 'square',}).appendTo('#grid');
 	};
+	$('<div>', {'class' : 'square',}).appendTo('#grid');
+    }    
+    
 
+    var boxSize = 960 / boxCount;
+    $('.square').css({
+	'width':boxSize,
+	'height':boxSize
+    });
 };
+
+
+chngClr = function(){
+    $square = $('.square')
+    $square.hover(function(){
+	$(this).addClass('hilite');
+    });
+}
